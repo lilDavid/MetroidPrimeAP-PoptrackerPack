72de6faecda924763804be8a45d6b12aa5f1bc76
lil David <1337lilDavid@gmail.com>
2024-08-16 11:58:06 -0500

    Add autotracking

---
+++ tools/generate_location_mappings.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Uses grep and sed to generate the location autotracking mappings as a Lua table
+
+if [[ "$#" -lt 1 ]]; then
+    >&2 echo "Usage: $0 apworld-path"
+    exit 1
+fi
+
+name="[A-Za-z ]+"
+location_name="'($name): ($name)( - ([A-Za-z -]+))?': ([0-9]+),?"
+
+exec 3> scripts/autotracking/location_mapping.lua
+echo 'LOCATION_MAPPING = {' >&3
+grep -E "$location_name" "$1/Locations.py" | sed -E "s|$location_name|[\\5] = {\"@\\1/\\2/\\4\"},|" >&3
+echo '}' >&3
+exec 3>&-
+
