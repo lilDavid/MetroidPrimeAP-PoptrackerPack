b0243bda8fff8802d1870f3647534489e7be169b
lil David <1337lilDavid@gmail.com>
2025-04-04 09:22:17 -0500

    UAT - respect auto tab

--- scripts/autotracking/uat/variable_mapping.lua
+++ scripts/autotracking/uat/variable_mapping.lua
@@ -2,6 +2,8 @@ VARIABLE_MAPPING = {
     ["Current Area"] = function(name, value)
         local level = value
         if not level then return nil end
+        local auto_tab = Tracker:FindObjectForCode("AutoTab")
+        if auto_tab == nil  or not auto_tab.Active then return nil end
         Tracker:UiHint("ActivateTab", level)
         return level
     end,
