0e1dc2a492429f13391a83692a3a225e27c44887
lil David <1337lilDavid@gmail.com>
2026-05-17 10:40:10 -0500

    Use the package UID for zipped pack

--- tools/zip-pack.sh
+++ tools/zip-pack.sh
@@ -2,11 +2,11 @@
 
 mkdir -p output
 
-rm -f MetroidPrimeAP_PopTrackerPack.zip
+rm -f MetroidPrimeAP_PopTrackerPack.zip MetroidPrime-Archipelago-lilDavid.zip
 rm -rf output/*
 
 files="images items layouts locations maps scripts var_* manifest.json settings.json"
 cp -r $files output
 
 cd output
-zip -r ../MetroidPrimeAP_PopTrackerPack.zip * -x "**/.DS_Store"
+zip -r ../MetroidPrime-Archipelago-lilDavid.zip * -x "**/.DS_Store"
