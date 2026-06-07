59f0b4384c80f24683c4123af20c509e902b89cf
lil David <1337lilDavid@gmail.com>
2026-06-07 09:58:02 -0500

    Print pack hash on build

--- tools/zip-pack.sh
+++ tools/zip-pack.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+PACK_NAME=MetroidPrime-Archipelago-lilDavid.zip
+
 mkdir -p output
 
-rm -f MetroidPrimeAP_PopTrackerPack.zip MetroidPrime-Archipelago-lilDavid.zip
+rm -f MetroidPrimeAP_PopTrackerPack.zip "$PACK_NAME"
 rm -rf output/*
 
 files="images items layouts locations maps scripts var_* manifest.json settings.json"
 cp -r $files output
 
 cd output
-zip -r ../MetroidPrime-Archipelago-lilDavid.zip * -x "**/.DS_Store"
+zip -r "../$PACK_NAME" * -x "**/.DS_Store"
+
+echo
+cd ..
+sha256sum "$PACK_NAME"
